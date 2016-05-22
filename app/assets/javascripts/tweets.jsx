4be65799a5ecdc0facc5d07a58051ac09c9921e0
Hutch Vivian <hutch.vivian@gmail.com>
2016-05-21 19:17:13 -0700

    fixed styling and added enter-press functionality

--- app/assets/javascripts/tweets.jsx
+++ app/assets/javascripts/tweets.jsx
@@ -38,15 +38,30 @@ $(function() {
     changeUserName: function(e) {
       this.setState({ username: e.target.value });
     },
+    onKeyPress: function(e) {
+      if (e.key == 'Enter') {
+        this.update();
+      }
+    },
     render: function() {
       return (
         <div>
         <div className="userSelector row">
           <div className="col-xs-8 col-md-12">
-            <input className="form-control" placeholder="Twitter Handle" type="text" id="username" onChange={this.changeUserName}/>
+            <input
+              className="form-control"
+              placeholder="Twitter Handle"
+              type="text"
+              id="username"
+              onKeyPress={this.onKeyPress}
+              onChange={this.changeUserName}/>
           </div>
           <div className="col-xs-4 col-md-12">
-            <button className="form-control btn btn-default" onClick={this.update}>Change User</button>
+            <button
+              className="form-control btn btn-default"
+              onClick={this.update}>
+              Change User
+            </button>
           </div>
         </div>
         <hr />
