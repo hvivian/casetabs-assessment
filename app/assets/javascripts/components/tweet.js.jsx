5b78228af091ae91b910c02b7655e065789967b1
Hutch Vivian <hutch.vivian@gmail.com>
2016-05-21 20:11:00 -0700

    added listening to hash event for username functionality

--- app/assets/javascripts/components/tweet.js.jsx
+++ app/assets/javascripts/components/tweet.js.jsx
@@ -1,16 +1,20 @@
 var Tweet = React.createClass({
   renderTweetText(text) {
-    markup = text.replace( /\s\@(\S+)/g, " <a href='#$1'>@$1</a>").replace( /\s\#(\S+)/g, " <b>#$1</b>");
+  markup =
+    text.replace(/\s?\#(\S+)/g, " <b>#$1</b>")
+        .replace(/^\#(\S+)/g, " <b>#$1</b>")
+        .replace(/\s?\@([a-zA-Z0-9_]+)/g, " <a href='#$1'>@$1</a>")
+        .replace(/^\@([a-zA-Z0-9_]+)/g, " <a href='#$1'>@$1</a>");
     return {__html: markup}; 
   },
   render: function() {
     return (
       <div className='tweet row'>
-        <div className="col-sm-3">
+        <div className="col-sm-4">
           <UserInfo user={this.props.tweet.user} />
         </div>
         <div
-          className="col-sm-9"
+          className="col-sm-8"
           dangerouslySetInnerHTML={ this.renderTweetText(this.props.tweet.text)  }>
         </div>
       </div>
--- app/assets/javascripts/components/tweets.jsx
+++ app/assets/javascripts/components/tweets.jsx
@@ -1,5 +1,14 @@
 $(function() {
 
-  React.render(<TweetList username="casetabs" />, document.getElementById('app'));
+  var render = function() {
+    var hash = location.hash.replace(/^#/, '');
+    var appNode = document.getElementById('app');
+    React.unmountComponentAtNode(appNode)
+    React.render(<TweetList username={ hash || "casetabs" } />, appNode);
+  };
+  
+  window.onhashchange = render;
+  window.onpopstate = render;
 
+  render();
 });
