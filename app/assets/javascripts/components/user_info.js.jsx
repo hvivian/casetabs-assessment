9565ed6e96f9075becca210e0fbee95c6ed03888
Hutch Vivian <hutch.vivian@gmail.com>
2016-05-21 21:02:08 -0700

    changed user info layout for small devices

--- app/assets/javascripts/components/user_info.js.jsx
+++ app/assets/javascripts/components/user_info.js.jsx
@@ -2,10 +2,10 @@ var UserInfo = React.createClass({
   render: function() {
     return (
       <div className='user row'>
-        <div className="col-md-4">
+        <div className="col-sm-4">
           <img src={this.props.user.profile_image_url} alt="user image" className="img-rounded" />
         </div>
-        <div className="col-md-8">
+        <div className="col-sm-8">
           <h5>{ this.props.user.name }</h5>
           <h6>@{ this.props.user.screen_name }</h6>
         </div>
