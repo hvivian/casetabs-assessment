7b9271413558e67185084ea9cc3acd8407dde1b3
Hutch Vivian <hutch.vivian@gmail.com>
2016-05-21 21:14:59 -0700

    synchronous tooltip loading

--- app/assets/javascripts/components/tweet.js.jsx
+++ app/assets/javascripts/components/tweet.js.jsx
@@ -1,21 +1,28 @@
 var Tweet = React.createClass({
+
   componentDidMount: function() {
     $(".user-hover").mouseover(function() {
-      $(".tooltip").remove();
       var self = this;
-      $.ajax({
-        url: "user?username=" + $(this).attr('href').slice(1),
-        dataType: 'json',
-        success: function(user) {
-          var markup = "<div>" + user.name + "</div>"
-          markup += "<div>Followers: " + user.followers_count + "</div>"
-          markup += "<div>Tweets: " + user.statuses_count + "</div>"
-          $(self).tooltip({title: markup, html: true}).tooltip('show');
-        },
-        error: function(xhr, status, err) {
-          console.error(status, err);
+      $(this).tooltip({
+        html: true,
+        title: function() {
+          var markup = ""
+          $.ajax({
+            async: false,
+            url: "user?username=" + $(self).attr('href').slice(1),
+            dataType: 'json',
+            success: function(user) {
+              markup += "<div>" + user.name + "</div>"
+              markup += "<div>Followers: " + user.followers_count + "</div>"
+              markup += "<div>Tweets: " + user.statuses_count + "</div>"
+            },
+            error: function(xhr, status, err) {
+              console.error(status, err);
+            }
+          });
+          return markup;
         }
-      });
+      }).tooltip('show');
     });
   },
   renderTweetText(text) {
@@ -24,6 +31,7 @@ var Tweet = React.createClass({
           .replace(/^\#(\S+)/g, " <b>#$1</b>")
           .replace(/\s?\@([a-zA-Z0-9_]+)/g, " <a class='user-hover' href='#$1'>@$1</a>")
           .replace(/^\@([a-zA-Z0-9_]+)/g, " <a class='user-hover' href='#$1'>@$1</a>");
+
     return {__html: markup}; 
   },
   render: function() {
